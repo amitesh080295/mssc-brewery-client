a3926668858f95a4dd1604aead42fdc4203fff0f
Amitesh_Sinha <amitesh_sinha@dell.com>
2020-10-11 15:58:55 +0530

    Initial Commit

---
+++ src/main/java/com/phasebeyond/msscbreweryclient/MsscBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.phasebeyond.msscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryClientApplication.class, args);
+	}
+
+}
